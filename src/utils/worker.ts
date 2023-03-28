f49ab98b1009b374c7ca2ae7cb1fa55ca134da3b
vishal <jsvishal2014@gmail.com>
2023-03-28 10:13:03 +0530

    git push

--- src/utils/worker.ts
+++ src/utils/worker.ts
@@ -13,44 +13,7 @@ const path = require('path');
 const mime = require('mime-types');
 
 const COMPANY_NAME = "nextgen";
-const contacts = [
-  {
-    contact: "8489767296",
-    name: "Winfred",
-    companyName: "NexGen Labs",
-  },
-
-  {
-    contact: "8089678967",
-    name: "Prabhu",
-    companyName: "NexGen Labs",
-  },
-  {
-    contact: "8249554989",
-    name: "Kushal",
-    companyName: "NexGen Labs",
-  },
-  {
-    contact: "7904172010",
-    name: "Nexgen meds",
-    companyName: "NexGen Labs",
-  },
-  {
-    contact: "9962581190",
-    name: "Karthik",
-    companyName: "NexGen Labs",
-  },
-  {
-    contact: "9790895396",
-    name: "Surya",
-    companyName: "NexGen Labs",
-  },
-  {
-    contact: "9790953563",
-    name: "Subramani",
-    companyName: "NexGen Labs",
-  },
-];
+const CONTACTS = contacts;
 
 const client = new Client({
     authStrategy: new LocalAuth({ clientId: "next-gen" }),
